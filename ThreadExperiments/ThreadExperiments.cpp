d0780a13a4d237d1b394e2e541b965f103775ac3
Arulanandan P <arulanandan@gmail.com>
2020-07-05 22:13:01 +0530

    Test commit

--- ThreadExperiments/ThreadExperiments.cpp
+++ ThreadExperiments/ThreadExperiments.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -12,6 +14,14 @@ class IWorker
     virtual void run() = 0;
 };
 
+class WorkerClass
+{
+public:
+    void operator() () const
+    {
+        cout << "\nInside thread Workerclass \n";
+    }
+};
 
 
 void worker2()
@@ -25,16 +35,81 @@ void worker()
     std::thread t1(&worker2);
     std::this_thread::sleep_for(std::chrono::seconds(2));
 
+    std::thread t3{ WorkerClass() };
+
     t1.join();
+    t3.join();
 
 }
+
+string dummy(int x)
+{
+    return to_string(x);
+}
+
+class threadGuard
+{
+    std::thread _t;
+public:
+    threadGuard(std::thread&& t):_t(std::move(t))
+    {
+        if (!_t.joinable())
+        {
+            throw "Not joinable thread";
+        }
+    }
+    ~threadGuard()
+    {
+        if (_t.joinable())
+        {
+            std::thread::id id = _t.get_id();
+            _t.join();
+            cout << "\n Joining thread id = " << id <<"\n";
+        }
+    }
+    threadGuard() = default;
+    threadGuard(threadGuard const&) = delete;
+    threadGuard& operator=(threadGuard const&) = delete;
+    threadGuard(threadGuard && other)
+    {
+        _t.swap(other._t);
+    }
+    threadGuard& operator=(threadGuard && t)
+    {
+        if (&t == this)
+        {
+            return *this;
+        }
+        _t.swap(t._t);
+
+        return *this;
+
+    }
+
+};
 int main()
 {
     cout << "Hardware threads = " << std::thread::hardware_concurrency() << endl;
     std::cout << "Hello World!\n";
-    std::thread t1(&worker);
-    t1.join();
-    cout << "Thread finished" << endl;
+    //std::thread t1(&worker);
+    //t1.join();
+    //cout << "Thread finished" << endl;
+    vector<threadGuard> threads(10);
+    for (int j = 0; j < 10; j++)
+    {
+        threads[j] = std::move( threadGuard(std::thread(
+            []()
+		    {
+				cout << "\nExecuting current thread id = " << std::this_thread::get_id() << "\n";
+				int k = 0;
+				for (int i = 0; i < 100000; i++)
+				{
+					k = i;
+				}
+		    }
+		    )));
+    }
+    cout << "end of main\n";
 
     return 0;
 }
